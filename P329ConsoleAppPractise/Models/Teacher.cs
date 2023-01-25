25227d36abf62ee0cacc279dbe39b64a43c353c9
Elvinp329 <elvinkj@code.edu.az>
2023-01-25 16:47:53 +0400

    PracticeP329

---
+++ P329ConsoleAppPractise/Models/Teacher.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace P329ConsoleAppPractise.Models
+{
+    internal class Teacher : Person
+    {
+        internal Group[] Groups { get; set; } = new Group[10];
+
+        internal string Profession { get; set; }
+
+        public override string ToString()
+        {
+            string groups = "";
+
+            foreach (var item in groups)
+            {
+                if (item == null)
+                    continue;
+
+                groups += item + "\n";
+            }
+
+            return $"{Id} {Firstname} {Lastname} {Age} \n {Profession} \n Groups :{groups}";
+        }
+
+
+
+    }
+}
